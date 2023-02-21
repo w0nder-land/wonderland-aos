e3fce589ffdf12c036a48639aa337da571269e89
Sangmeebee <apfhdznzl@gmail.com>
2023-02-22 00:56:48 +0900

    feat : 디자인시스템 제작

---
+++ core-designsystem/build.gradle.kts
@@ -0,0 +1,45 @@
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+}
+
+
+android {
+    compileSdk = 33
+
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 33
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+
+    AndroidConfig.run {
+        implementation(CORE_KTX)
+        implementation(APPCOMPAT)
+        implementation(MATERIAL)
+        implementation(CONSTRAINT_LAYOUT)
+        implementation(SWIPE_REFRESH_LAYOUT)
+        implementation(FRAGMENT_KTX)
+    }
+}
