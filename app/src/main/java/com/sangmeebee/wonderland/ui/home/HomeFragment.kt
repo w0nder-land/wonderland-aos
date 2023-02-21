8120a3d3ab58da2074da9367a30c505f7d4926d9
Sangmeebee <apfhdznzl@gmail.com>
2023-02-22 02:13:01 +0900

    refactor: 기존 불필요한 코드 제거

--- app/src/main/java/com/sangmeebee/wonderland/ui/home/HomeFragment.kt
+++
@@ -1,6 +0,0 @@
-package com.sangmeebee.wonderland.ui.home
-
-import com.sangmeebee.wonderland.databinding.FragmentHomeBinding
-import com.sangmeebee.wonderland.ui.base.BaseFragment
-
-class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate)
