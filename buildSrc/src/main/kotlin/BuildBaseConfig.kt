7548994a8d795a929cef5658d3bc9fcb1090e4ab
김한식 <dev.imaec@gmail.com>
2023-04-26 15:18:29 +0900

    ♻️ 카카오 로그인 native app key 변경

--- buildSrc/src/main/kotlin/BuildBaseConfig.kt
+++ buildSrc/src/main/kotlin/BuildBaseConfig.kt
@@ -4,4 +4,5 @@ object BuildBaseConfig {
     const val COMPILE_VERSION = 33
     const val TARGET_SDK_VERSION = 33
     const val BUILD_TOOLS_VERSION = "31.0.0"
+    const val KAKAO_NATIVE_APP_KEY = "fde8294d68365c30cf489c02ba5f88e3"
 }
